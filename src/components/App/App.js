a07fc1e813cfe942cddf7d5a1c00251b84553c1c
Vasily Muraviev <muravievvv@develop.mos.ru>
2022-10-07 01:02:39 +0300

    about block

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import '../../index.css';
 import Header from '../Header/Header';
 import Intro from '../Intro/Intro';
+import About from '../About/About';
 
 function App() {
     return (
         <div className="app">
             <Header />
             <Intro />
+            <About />
         </div>
     );
 }
